62d802b6aba19ff6e142b3baefc44634b92a0f8d
SonKn1ght <maximiilian321@gmail.com>
2020-11-02 11:06:16 +0300

    feat: deploy

--- js/script.js
+++
@@ -1,6 +0,0 @@
-let navToggle = document.querySelector('.main-nav__toggle');
-let navMainList = document.querySelector('.main-nav__list');
-
-navToggle.onclick = function() {
-  navMainList.classList.toggle('main-nav__list-open');
-};
